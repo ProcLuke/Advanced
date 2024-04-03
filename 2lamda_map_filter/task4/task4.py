fad97cafb113f5e5bf026098ba7380ee501a1201
Lukas Prochazka <pro41216@spseol.cz>
2024-04-03 14:16:10 +0200

    4

--- 2lamda_map_filter/task4/task4.py
+++ 2lamda_map_filter/task4/task4.py
@@ -1,2 +1,2 @@
 def complex_word_filter(words, min_length):
-    pass
+    return list(map(lambda w: w.upper() + str(w.count("a") + w.count("e") + w.count("i") + w.count("o") + w.count("u")), filter(lambda x: len(x)>min_length,words)))
