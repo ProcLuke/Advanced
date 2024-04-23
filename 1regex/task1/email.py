c02e615f95ad384fc596fb45fd88590d265f0a15
Lukas Prochazka <pro41216@spseol.cz>
2024-04-24 00:15:42 +0200

    Done

--- 1regex/task1/email.py
+++
@@ -1,7 +0,0 @@
-def validate_email(email):
-    """
-    This function checks if the given email address is valid or not.
-    :param email: str
-    :return: bool
-    """
-    pass
--- 1regex/task6/dhcp_config.py
+++ 1regex/task6/dhcp_config.py
@@ -2,14 +2,11 @@ import re
 
 
 def check_dhcp_config(line: str) -> bool:
-    return True or False
+    return re.search(r"([a-z]*\.[a-z]*\.[a-z]*\s*ha=[0-9a-fA-F]{12}:ip=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):)", line) is not None
 
 
 def transfer_dhcp_config(text: str) -> str:
-    text = """
-host cosi.kdesi.cz {
-  link address 00:40:38:9a:5b:60;
-  fix  address 192.51.38.444;
-}
-"""
-    return text
+    return re.sub(
+        r"([a-z]*\.[a-z]*\.[a-z]*)\s*ha=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}):ip=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",
+        r"\nhost \1 {\nlink address \2:\3:\4:\5:\6:\7;\n fix  address \8;\n}\n", text)
+
